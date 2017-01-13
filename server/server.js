a8d0ce2702083d2729bcf46cd7487f20b51e6520
PSIB0T <arvindonarayanan@gmail.com>
2017-01-13 17:13:15 +0530

    Validators types and defaults

--- server/server.js
+++ server/server.js
@@ -3,13 +3,18 @@ mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost:27017/TodoApp');
 var todoModel = mongoose.model('todoModel', {
   text : {
-    type : String
+    type : String,
+    required: true,
+    minlength: 3,
+    trim: true
   },
   completed : {
-    type : Boolean
+    type : Boolean,
+    default: false
   },
   completedAt : {
-    type : Number
+    type : Number,
+    default: null
   }
 });
 // var newTodo = new todoModel({
@@ -22,14 +27,30 @@ var todoModel = mongoose.model('todoModel', {
 //   console.log('Unable to add new todo document to database');
 // });
 
-newTodo = new todoModel({
-  text : 'Code!',
-  completed : true,
-  completedAt: 251
+// newTodo = new todoModel({
+//   text: '  Hello mongoose  '
+// });
+
+// newTodo.save().then((doc) => {
+//   console.log('Saved document\n', doc);
+// }, (err) => {
+//   console.log('Unable to add new todo document to database', err);
+// });
+
+var user = mongoose.model('User', {
+  email: {
+    type: String,
+    trim: true,
+    minlength: 1
+  }
 });
+
+newTodo = new user({
+  email: '  hello@gmail.com  '
+})
 
 newTodo.save().then((doc) => {
   console.log('Saved document\n', doc);
 }, (err) => {
-  console.log('Unable to add new todo document to database');
+  console.log('Unable to add new todo document to database', err);
 });
