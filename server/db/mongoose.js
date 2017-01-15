3d1959d32299d7dafbdc2ff1611cf545606c441d
PSIB0T <arvindonarayanan@gmail.com>
2017-01-15 12:11:02 +0530

    Deploying app to heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+mongoose.connect('mongodb://todouser:$TheSpecialUser$@ds145828.mlab.com:45828/todoapp');
 
 module.exports = {
   mongoose
